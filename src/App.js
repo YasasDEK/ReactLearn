530715fde77fa6498911615865c55ed2cc4f77b0
yasas <ydilshan.ek@gmail.com>
2020-12-14 13:03:48 +0530

    learn react project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,376 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import x, { firstname, lastname } from './test';
+import PropTypes from 'prop-types';
+import { sortBy } from 'lodash';
+import classNames from 'classnames';
 
-function App() {
+const DEFAULT_QUERY = '';
+const DEFAULT_PAGE = 0;
+const DEFAULT_HPP = '10 ';
+
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
+
+// const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
+
+const userList = ['Robin', 'Andrew', 'Dan'];
+const additionalUser = ['Jordan','Yasas'];
+const allUsers = [ ...userList, ...additionalUser ];
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+  ]
+
+  const SORTS = {
+    NONE: list => list,
+    TITLE: list => sortBy(list, 'title'),
+    AUTHOR: list => sortBy(list, 'author'),
+    COMMENTS: list => sortBy(list, 'num_comments').reverse(),
+    POINTS: list => sortBy(list, 'points').reverse(),
+  };
+
+  function isSearched(searchTerm) {
+    return function(item) {
+    return !searchTerm ||
+    item.title.toLowerCase().includes(searchTerm.toLowerCase());
+    }
+  }
+
+  const withFoo = (Component) => (props) =>
+    <Component { ...props } />
+  
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      results: null,
+      searchKey: '',
+      searchTerm: DEFAULT_QUERY,
+      isLoading: false,
+      sortKey: 'NONE',
+      isSortReverse: false,
+    }
+
+    this.needsToSearchTopstories = this.needsToSearchTopstories.bind(this);
+    this.setSearchTopstories = this.setSearchTopstories.bind(this);
+    this.fetchSearchTopstories = this.fetchSearchTopstories.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
+    this.onSort = this.onSort.bind(this);
+  }
+
+  onSort(sortKey) {
+    const isSortReverse = this.state.sortKey === sortKey && !this.state.isSortReverse;
+    this.setState({ sortKey, isSortReverse });
+  }
+  
+  setSearchTopstories(result) {
+    const { hits, page } = result;
+    const { searchKey, results } = this.state;
+    const oldHits = results && results[searchKey]
+      ? results[searchKey].hits
+      : [];
+    
+    const updatedHits = [
+      ...oldHits,
+      ...hits
+    ];
+    
+    this.setState({
+    results: {
+        ...results,
+        [searchKey]: { hits: updatedHits, page }
+      },
+      isLoading: false
+    });
+  }
+
+  needsToSearchTopstories(searchTerm) {
+    return !this.state.results[searchTerm];
+  }
+
+  fetchSearchTopstories(searchTerm, page) {
+    this.setState({ isLoading: true });
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
+    .then(response => response.json())
+    .then(result => this.setSearchTopstories(result));
+  }
+
+  onSearchSubmit(event) {
+    const { searchTerm } = this.state;
+    this.setState({ searchKey: searchTerm });
+    
+    if (this.needsToSearchTopstories(searchTerm)) {
+      this.fetchSearchTopstories(searchTerm, DEFAULT_PAGE);
+    }
+    
+    event.preventDefault();
+  }
+
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    this.setState({ searchKey: searchTerm });
+    this.fetchSearchTopstories(searchTerm, DEFAULT_PAGE);
+  }
+
+  onDismiss(id) {
+    const { searchKey, results } = this.state;
+    const { hits, page } = results[searchKey];
+    const isNotId = item => item.objectID !== id;
+    const updatedHits = hits.filter(isNotId);
+    this.setState({
+      results: {
+      ...results,
+      [searchKey]: { hits: updatedHits, page }
+      }
+    });
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+    console.log(event.target.value);
+  }
+
+  render() {
+    console.log(React.version)
+    console.log('->' + x.firstname + ' ' + lastname);
+    const {
+      searchTerm,
+      results,
+      searchKey,
+      isLoading,
+      sortKey,
+      isSortReverse
+    } = this.state;
+
+    const page = (
+      results &&
+      results[searchKey] &&
+      results[searchKey].page
+    ) || 0;
+    
+    const list = (
+      results &&
+      results[searchKey] &&
+      results[searchKey].hits
+    ) || [];
+    
+    return (
+      <div className="page">
+        <div className="interactions">
+          <Search
+            value={searchTerm}
+            onChange={this.onSearchChange}
+            onSubmit={this.onSearchSubmit}>
+            Search
+          </Search>
+        </div>
+        <Table
+          list={list}
+          sortKey={sortKey}
+          isSortReverse={isSortReverse}
+          onSort={this.onSort}
+          onDismiss={this.onDismiss}/>
+        <div className="interactions">
+          <ButtonWithLoading
+            isLoading={isLoading}
+            onClick={() => this.fetchSearchTopstories(searchKey, page + 1)}>
+              More
+          </ButtonWithLoading>
+        </div>
+      </div>
+    );
+  }
+}
+
+const Search = ({
+    value,
+    onChange,
+    onSubmit,
+    children
+  }) => {
+  let input;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        value={value}
+        onChange={onChange}
+        ref={(node) => input = node}/>
+      <button type="submit">
+      {children}
+      </button>
+    </form>
+  );
+}
+
+const Table = ({
+    list,
+    sortKey,
+    isSortReverse,
+    onSort,
+    onDismiss
+  }) => {
+    const sortedList = SORTS[sortKey](list);
+    const reverseSortedList = isSortReverse
+    ? sortedList.reverse()
+    : sortedList;
+
+  return( 
+    <div className="table">
+      <div className="table-header">
+        <span style={{ width: '40%' }}>
+          <Sort
+            sortKey={'TITLE'}
+            onSort={onSort}
+            activeSortKey={sortKey}>
+            Title
+          </Sort>
+        </span>
+        <span style={{ width: '30%' }}>
+          <Sort
+            sortKey={'AUTHOR'}
+            onSort={onSort}
+            activeSortKey={sortKey}>
+              Author
+          </Sort>
+        </span>
+        <span style={{ width: '10%' }}>
+          <Sort
+            sortKey={'COMMENTS'}
+            onSort={onSort}
+            activeSortKey={sortKey}>
+              Comments
+          </Sort>
+        </span>
+        <span style={{ width: '10%' }}>
+          <Sort
+            sortKey={'POINTS'}
+            onSort={onSort}
+            activeSortKey={sortKey}>
+              Points
+          </Sort>
+        </span>
+        <span style={{ width: '10%' }}>
+          Archive
+        </span>
+      </div>
+
+      { reverseSortedList.map(item =>
+        <div key={item.objectID} className="table-row">
+          <span style={{ width: '40%' }}>
+          <a href={item.url}>{item.title}</a>
+          </span>
+          <span style={{ width: '30%' }}>
+            {item.author}
+          </span>
+          <span style={{ width: '10%' }}>
+            {item.num_comments}
+          </span>
+          <span style={{ width: '10%' }}>
+            {item.points}
+          </span>
+        <span style={{ width: '10%' }}>
+          <Button
+            onClick={() => onDismiss(item.objectID)}
+            className="button-inline">
+            Dismiss
+          </Button>
+        </span>
+      </div>
+      )}
     </div>
   );
 }
 
+Table.propTypes = {
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+    objectID: PropTypes.string.isRequired,
+    author: PropTypes.string,
+    url: PropTypes.string,
+    num_comments: PropTypes.number,
+    points: PropTypes.number,
+    })
+  ).isRequired,
+  // onDismiss: PropTypes.func.isRequired,
+};
+
+const Button = ({ onClick, className, children }) =>
+  <button
+    onClick={onClick}
+    className={className}
+    type="button">
+    {children}
+  </button>
+
+const Loading = () =>
+  <div>Loading ...</div>
+
+const withLoading = (Component) => ({ isLoading, ...rest }) =>
+  isLoading ? <Loading /> : <Component { ...rest } />
+
+const ButtonWithLoading = withLoading(Button);
+
+Button.defaultProps = {
+  className: '',
+};
+
+Button.propTypes = {
+  // onClick: PropTypes.func.isRequired,
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired,
+};
+
+const Sort = ({
+    sortKey,
+    activeSortKey,
+    onSort,
+    children
+  }) => {
+    const sortClass = classNames(
+      'button-inline',
+      { 'button-active': sortKey === activeSortKey }
+    );
+ 
+  // if (sortKey === activeSortKey) {
+  //  sortClass.push('button-active');
+  // }
+
+  return (
+    <Button
+      onClick={() => onSort(sortKey)}
+      className={sortClass}>
+        {children}
+    </Button>
+  );
+}
+
 export default App;
+
+export {
+  Button,
+  Search,
+  Table,
+  };
--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App></App>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+if (module.hot) {
+  module.hot.accept()
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
